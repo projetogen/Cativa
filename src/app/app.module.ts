af203c655de8cfa8da36ece59512ea192941b1a5
Murilo Feitosa <muriloalf@gmail.com>
2022-01-20 15:31:58 -0300

    @MuriloAugusto1 [Task: creating first section of the landing page]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Section1LandingpageComponent } from './section1-landingpage/section1-landingpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Section1LandingpageComponent
   ],
   imports: [
     BrowserModule,
