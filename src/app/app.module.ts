dec308e0f0dfbecca2d0311678a16e2694ba9208
mariana-nogueira21 <mariana.nogueira21@gmail.com>
2022-01-20 20:01:50 -0300

    @mariana-nogueira21 [Task: implementação do componente tema]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemaComponent } from './tema/tema.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemaComponent
   ],
   imports: [
     BrowserModule,
