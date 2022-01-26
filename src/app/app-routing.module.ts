78d5e7028bdcd1ea1d973b7b12ff21efcbdaff79
Rafael Borges <rafaelborges.av@gmail.com>
2022-01-26 16:15:50 -0300

    @okborges[update rotas]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CadastrarComponent } from './cadastrar/cadastrar.component';
 
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { LoginComponent } from './login/login.component';
 import { SobreComponent } from './sobre/sobre.component';
 import { TemaComponent } from './tema/tema.component';
 
@@ -11,10 +12,12 @@ const routes: Routes = [
   {path: '',redirectTo: 'entrar', pathMatch: 'full'},
 
 
-  {path: 'cadastar', component: CadastrarComponent},
- 
+  {path: 'entrar', component: LoginComponent},
+
+  {path: 'cadastrar', component: CadastrarComponent},
+
   {path: 'tema', component: TemaComponent},
- 
+
   {path: 'sobre', component: SobreComponent}
 ];
 
