f10eba970e269a3de8942a88a0b130687f1d61e3
Vivian-Santana <santanavivian245@gmail.com>
2022-02-01 23:00:03 -0300

    @Vivian-Santana [task: postagem delete]

---
+++ src/app/delete/postagem-delete/postagem-delete.component.ts
@@ -0,0 +1,49 @@
+import { environment } from './../../../environments/environment.prod';
+import { PostagemService } from './../../service/postagem.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { PostagemModel } from 'src/app/model/PostagemModel';
+
+@Component({
+  selector: 'app-postagem-delete',
+  templateUrl: './postagem-delete.component.html',
+  styleUrls: ['./postagem-delete.component.css']
+})
+export class PostagemDeleteComponent implements OnInit {
+
+  postagem: PostagemModel = new PostagemModel()
+  idPost: number
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private postagemService: PostagemService,
+  ) { }
+
+  ngOnInit() {
+
+    window.scroll(0,0)
+
+    if(environment.token == ''){
+      this.router.navigate(['/entrar'])
+    }
+
+    this.idPost = this.route.snapshot.params['id']
+    this.findByIdPostagem(this.idPost)
+  }
+
+  findByIdPostagem(id: number){
+    this.postagemService.getByIdPostagem(id).subscribe((resp: PostagemModel) => {
+      this.postagem = resp
+    })
+  }
+
+
+  apagar(){
+    this.postagemService.deletePostagem(this.idPost).subscribe(()=>{
+      alert('Postagem apagada com sucesso!')
+      this.router.navigate(['/inicio'])
+    })
+  }
+
+}
